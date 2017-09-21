6a17f12742f07b61adbdc6e187c22d62860aa2f0
Christian Ruink <cruink@nearsoft.com>
2017-09-21 13:59:26 -0700

    Fixed helpers tests

--- spec/helpers/documentation_helper_spec.rb
+++ spec/helpers/documentation_helper_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
-describe DaylightDocumentation::DocumentationHelper do
+RSpec.describe DaylightDocumentation::DocumentationHelper do
+  include DaylightDocumentation::DocumentationHelper
 
   class TestModel < ActiveRecord::Base
     include Daylight::Associations
@@ -30,53 +31,53 @@ describe DaylightDocumentation::DocumentationHelper do
     it "yields the route verb, path, and defaults hash for the model" do
       yielded = false
       verbs = %w[GET POST PUT PATCH DELETE]
-      helper.model_verbs_and_routes TestModel do |verb, path, defaults|
+      model_verbs_and_routes TestModel do |verb, path, defaults|
         verbs.should include(verb)
         path.to_s.should =~ %r{/test_model}
         defaults.should be_a(Hash)
         yielded = true
       end
 
-      yielded.should be_true
+      yielded.should be_truthy
     end
   end
 
   describe :model_filters do
     it "returns list of the model's filters including attributes" do
-      helper.model_filters(TestModel).should include('name')
+      model_filters(TestModel).should include('name')
     end
 
     it "returns list of the model's filters including associations" do
-      helper.model_filters(TestModel).should include('users')
+      model_filters(TestModel).should include('users')
     end
   end
 
   describe :action_definition do
     it "returns a definition for an action" do
-      helper.action_definition({action:'index'}, TestModel).should ==
+      action_definition({action:'index'}, TestModel).should ==
         "Retrieves a list of test models"
     end
 
     it "handles associated" do
-      helper.action_definition({action:'associated', associated:'foo_bar'}, TestModel).should ==
+      action_definition({action:'associated', associated:'foo_bar'}, TestModel).should ==
         "Returns test model's foo bars"
     end
 
     it "handles remoted" do
-      helper.action_definition({action:'remoted', remoted:'baz'}, TestModel).should ==
+      action_definition({action:'remoted', remoted:'baz'}, TestModel).should ==
         "Calls test model's remote method baz"
     end
   end
 
   describe :client_namespace do
     it "returns the client namespace" do
-      helper.client_namespace.should == 'test_api'
+      client_namespace.should == 'test_api'
     end
   end
 
   describe :api_version do
     it "returns the api version" do
-      helper.api_version.should == 'v1'
+      api_version.should == 'v1'
     end
   end
 end
